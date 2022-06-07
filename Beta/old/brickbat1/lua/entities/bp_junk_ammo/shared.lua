80d8e3ad098d3ceba221fd7b576a58277cb7e468
Breavy <37123222+BreavyTF2@users.noreply.github.com>
2022-06-07 06:24:11 -0400

    7Zipped

--- Beta/old/brickbat1/lua/entities/bp_junk_ammo/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_entity"
-ENT.PrintName = "Cremator Heads"
-ENT.Author = "Battlepope"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true 
-ENT.Category = "Half-Life 2 Leak Ammo"
-ENT.AmmoType = "bp_junk"
-ENT.Amount = 1
